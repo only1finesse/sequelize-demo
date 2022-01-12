16cf8f6828b622429304c4cc1386d94253360337
only1finesse <72461240+only1finesse@users.noreply.github.com>
2022-01-12 08:18:28 -0800

    Adds files for Game.js and User.js

---
+++ models/Game.js
@@ -0,0 +1,16 @@
+const { db, DataTypes, Model } = require('../db');
+
+//create a game model ---> create a child class
+
+class Game extends Model {}; 
+
+//init to create a game table (model)
+
+Game.init({
+    name: DataTypes.STRING, 
+    platform: DataTypes.STRING
+}, {
+    sequelize: db
+})
+
+module.exports = Game
